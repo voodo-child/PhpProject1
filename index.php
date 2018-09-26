913bc0390494f725e8ee2e0ba4d17827c1576af9
voodo-child <33820738+voodo-child@users.noreply.github.com>
2018-09-25 22:27:31 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -15,25 +15,48 @@ and open the template in the editor.
         $address = filter_input(INPUT_POST,"address",FILTER_SANITIZE_SPECIAL_CHARS);
         $city = filter_input(INPUT_POST, "city", FILTER_SANITIZE_SPECIAL_CHARS);
         $postalCode = filter_input(INPUT_POST, "postalCode", FILTER_SANITIZE_SPECIAL_CHARS);
-        
-        
+        $flagName= false;  // True flag shows link to next page
+        $flagAddress= false;
+        $flagCity= false;
+        $flagPostalCode= false;
         if(preg_match('/^([0-9A-Z]\w*\s?)+$/', $storeName)){
+            $flagName=true;
             echo "<p>Store Name acceptable</p>";
+        } else{
+            $flagName=false;
+            echo"<p>Store Name is Invalid</p>";
         }
         
         if(preg_match('/\d+\s[A-Z][a-z]+\s(st|rd|ave|blv|St|Rd|Ave|Blv)\s?(E|W|S|N)?/', $address)){
+            $flagAddress=true;
             echo"<p>Address acceptable</p><br>";
+        } else{
+            $flagAddress=false;
+            echo "<p>Address is Invalid</p>";
         }
         
         if(preg_match('/^[A-Z][a-z]+$/',$city)){
+            $flagCity=true;
             echo "<p> City is Acceptable </p>";
+        } else {
+            $flagCity=false;
+            echo "<p> City is Invalid</p>";
         }
         
         if(preg_match('/^[a-zA-Z][0-9][a-zA-Z]\s?[0-9][a-zA-Z][0-9]$/', $postalCode)){
+            $flagPostalCode=true;
             echo "<p> Postal Code is acceptable </p>";
+        } else{
+            $flagPostalCode=false;
+            echo "<p>Postal Code is Invalid</p>";
         }
         
-        
+        if($flagName && $flagAddress&&$flagCity&&$flagPostalCode){
+            echo"<p>link to go on</p>";
+        } else{
+            echo"<p>Sorry Please Fix your inputs</p>";
+        }
+            
         
         ?>
     </body>
